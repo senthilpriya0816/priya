f469b07beb3ef58fd97d3125653fee7aa7bc49b9
senthilpriya0816 <144892715+senthilpriya0816@users.noreply.github.com>
2023-09-27 10:46:08 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,19 +1,16 @@
-year = 2023
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+  return indices
 
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
 
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
